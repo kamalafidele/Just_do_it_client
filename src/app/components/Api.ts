eb4f6d01ccbb0232ec4560316be11c1dc6f8ed62
unknown <Kamalafizzet45@gmail.com>
2021-12-24 12:22:08 +0200

    new changes

--- src/app/components/Api.ts
+++ src/app/components/Api.ts
@@ -2,5 +2,5 @@
 //OFFLINE API:"http://localhost:5000/api/justdoit"
 //ONLINE API:https://justdoitrw.herokuapp.com/api/justdoit 
 export const MYAPIS={
-    LIVEAPI:"http://localhost:5000/api/justdoit"
+    LIVEAPI:"https://justdoitrw.herokuapp.com/api/justdoit"
 }
